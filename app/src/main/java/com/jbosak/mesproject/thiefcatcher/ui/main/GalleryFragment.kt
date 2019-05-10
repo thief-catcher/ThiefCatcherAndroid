6c6c89e843b30b733c694c9a9eb50b37f6fd49a2
jBosak98 <jbosak98@gmail.com>
2019-05-10 12:23:51 +0200

    removed unnecessary code, gallery and liveFragment init

---
+++ app/src/main/java/com/jbosak/mesproject/thiefcatcher/ui/main/GalleryFragment.kt
@@ -0,0 +1,21 @@
+package com.jbosak.mesproject.thiefcatcher.ui.main
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.jbosak.mesproject.thiefcatcher.R
+
+class GalleryFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.gallery_fragment, container, false)
+    }
+
+
+
+}
