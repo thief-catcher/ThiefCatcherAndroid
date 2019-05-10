a76a6b227bcefb6c3c52b71bf2c56a4f6276f0c2
jBosak98 <jbosak98@gmail.com>
2019-05-10 14:19:26 +0200

    gallery ui

--- app/src/main/java/com/jbosak/mesproject/thiefcatcher/ui/main/GalleryFragment.kt
+++ app/src/main/java/com/jbosak/mesproject/thiefcatcher/ui/main/GalleryFragment.kt
@@ -2,20 +2,41 @@ package com.jbosak.mesproject.thiefcatcher.ui.main
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.FrameLayout
+import android.widget.GridView
 import com.jbosak.mesproject.thiefcatcher.R
 
 class GalleryFragment : Fragment() {
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<ImageAdapter.ImageViewHolder>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.gallery_fragment, container, false)
+        val view = inflater.inflate(R.layout.gallery_fragment, container, false)
+        viewManager = LinearLayoutManager(view.context)
+        val images = arrayOf("aaa", "bbb", "cccc", "ddddddddddddddddddddddddd")
+        viewAdapter = ImageAdapter(images)
+        recyclerView = view.findViewById<RecyclerView>(R.id.gallery_recycler_view).apply{
+            setHasFixedSize(true)
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
+
+
+
+        return view
     }
 
 
 
 }
+
--- app/src/main/java/com/jbosak/mesproject/thiefcatcher/ui/main/ImageAdapter.kt
+++ app/src/main/java/com/jbosak/mesproject/thiefcatcher/ui/main/ImageAdapter.kt
@@ -1,2 +1,32 @@
 package com.jbosak.mesproject.thiefcatcher.ui.main
 
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.jbosak.mesproject.thiefcatcher.R
+
+class ImageAdapter(private val images: Array<String>): RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, p1: Int): ImageViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val cell = layoutInflater.inflate(R.layout.gallery_card_view, parent, false)
+        return ImageViewHolder(cell)
+    }
+
+    override fun getItemCount(): Int = images.size
+
+    override fun onBindViewHolder(holder: ImageViewHolder, p1: Int) {
+        holder.date.text = images[p1]
+    }
+
+    class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view){
+        val date: TextView = itemView.findViewById(R.id.gallery_date_image)
+        val img: ImageView = itemView.findViewById(R.id.gallery_image)
+    }
+
+
+
+}
