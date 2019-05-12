f0d81d767b3ee1ae7cbdc18883cb6afc28bdb661
jBosak98 <jbosak98@gmail.com>
2019-05-12 15:16:03 +0200

    filenames formatted

--- app/src/main/java/com/jbosak/mesproject/thiefcatcher/ui/main/ImageAdapter.kt
+++ app/src/main/java/com/jbosak/mesproject/thiefcatcher/ui/main/ImageAdapter.kt
@@ -1,6 +1,6 @@
 package com.jbosak.mesproject.thiefcatcher.ui.main
 
-import android.graphics.Bitmap
+import android.annotation.SuppressLint
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +9,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.jbosak.mesproject.thiefcatcher.CapturedImage
 import com.jbosak.mesproject.thiefcatcher.R
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ImageAdapter(private val images: MutableList<CapturedImage>): RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
 
@@ -21,7 +23,11 @@ class ImageAdapter(private val images: MutableList<CapturedImage>): RecyclerView
 
     override fun getItemCount(): Int = images.size
 
+    @SuppressLint("SimpleDateFormat")
     override fun onBindViewHolder(holder: ImageViewHolder, p1: Int) {
+        val date : Date = SimpleDateFormat("yyyy-MM-DD'T'hhmmss.SSSS").parse(images[p1].name)
+
+        holder.date.text = SimpleDateFormat("yyyy-MM-DD  hh:mm:ss").format(date)
         holder.date.text = images[p1].name
 
         holder.img.setImageBitmap(images[p1].img)
