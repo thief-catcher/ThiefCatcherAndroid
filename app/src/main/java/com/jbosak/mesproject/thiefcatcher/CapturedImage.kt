e10781809267674097d1e6cc0f2e5559076e4fc7
jBosak98 <jbosak98@gmail.com>
2019-05-17 19:06:42 +0200

    CapturedImage equals and hashCode

--- app/src/main/java/com/jbosak/mesproject/thiefcatcher/CapturedImage.kt
+++ app/src/main/java/com/jbosak/mesproject/thiefcatcher/CapturedImage.kt
@@ -2,4 +2,21 @@ package com.jbosak.mesproject.thiefcatcher
 
 import android.graphics.Bitmap
 
-data class CapturedImage(val name: String, var img: Bitmap)
+data class CapturedImage(val name: String, var img: Bitmap){
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as CapturedImage
+
+        if (name != other.name) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = name.hashCode()
+        result = 31 * result + img.hashCode()
+        return result
+    }
+}
