817d9e593cdb45b39fc5563acf70fd9a9452f899
jBosak98 <jbosak98@gmail.com>
2019-05-28 13:25:12 +0200

    scheduleAlarm and AlarmReceiver

--- app/src/main/java/com/jbosak/mesproject/thiefcatcher/MainActivity.kt
+++ app/src/main/java/com/jbosak/mesproject/thiefcatcher/MainActivity.kt
@@ -1,14 +1,16 @@
 package com.jbosak.mesproject.thiefcatcher
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
-import android.support.design.widget.Snackbar
 import android.support.design.widget.TabLayout
 import android.support.v4.view.ViewPager
 import android.support.v7.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
 import android.view.WindowManager
+import com.jbosak.mesproject.thiefcatcher.alarm.AlarmReceiver
 import com.jbosak.mesproject.thiefcatcher.ui.main.SectionsPagerAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -24,11 +26,29 @@ class MainActivity : AppCompatActivity() {
         val fab: FloatingActionButton = findViewById(R.id.fab)
 
         val imgService = ImageService(this)
-        fab.setOnClickListener { view ->
+        fab.setOnClickListener {
             imgService.captureImage{ sectionsPagerAdapter.notifyDataSetChanged() }
         }
 
         supportActionBar?.hide()
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        scheduleAlarm()
     }
+
+    private fun scheduleAlarm(){
+        val intent = Intent(applicationContext, AlarmReceiver::class.java)
+        val pIntent = PendingIntent.getBroadcast(
+            this,
+            AlarmReceiver.REQUEST_CODE,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+        val firstMillis = System.currentTimeMillis()
+
+        val interval = 1000L
+        val alarm = this.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis, interval, pIntent)
+    }
+
 }
---
+++ app/src/main/java/com/jbosak/mesproject/thiefcatcher/alarm/AlarmReceiver.kt
@@ -0,0 +1,16 @@
+package com.jbosak.mesproject.thiefcatcher.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class AlarmReceiver:BroadcastReceiver(){
+    companion object {
+        const val REQUEST_CODE = 12345
+    }
+
+    override fun onReceive(context: Context?, intent: Intent?) {
+
+    }
+
+}
